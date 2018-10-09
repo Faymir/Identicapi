3801cba2646c306fb1ed3b91119566840a2e45a0
faymir <fayze@tutamail.com>
2018-10-09 16:38:52 +0100

    change route from image to img (shorter)

--- lib/identicapi_web/router.ex
+++ lib/identicapi_web/router.ex
@@ -11,7 +11,7 @@ defmodule IdenticapiWeb.Router do
     get("/:id", KernelController, :show)
   end
 
-  scope "/image", IdenticapiWeb do
+  scope "/img", IdenticapiWeb do
     pipe_through :api
     get "/:id/:width/:height", KernelController, :normal
     get "/:id", KernelController, :normal
